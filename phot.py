11b8b133e0ec4ca637ca299b03cc347e085dba90
Hector Salas O <hector.salas.o@gmail.com>
2019-07-24 16:58:03 -0400

    adds the use of cordinates in (ra,dec). fixed bug

--- phot.py
+++ phot.py
@@ -1,62 +1,89 @@
+import warnings
+import numpy as np
+import matplotlib.pyplot as plt
 from astropy.io import fits
+from astropy.wcs import WCS
 from astropy.table import Table
-from photutils import centroid_sources, CircularAperture, CircularAnnulus
-from photutils import aperture_photometry
+from photutils import CircularAperture, CircularAnnulus
+# from photutils import SkyCircularAperture, SkyCircularAnnulus
+from photutils import centroid_sources, aperture_photometry
 from astropy.stats import sigma_clipped_stats
 from astropy.visualization import simple_norm
+from astropy.coordinates import SkyCoord
+from astropy.utils.exceptions import AstropyUserWarning
 
-def ap_phot(image, sources_pos, source_ap, sky_ap, delta_ann=3.0,
-            centroid=False):
-    """Performs circular aperture fotometry on a image, given the x,y position of the sources.
+
+def ap_phot(image, sources, source_ap, sky_ap, delta_ann=3.0, coords='xy',
+            centroid=False, plot=False, **kargs):
+    """Performs circular aperture fotometry on a image, given a table with the
+    coordinates (x,y) or (ra,dec) of the sources.
 
     Input:
-        image: Image data.
-        sources_pos: Positions of the sources in the image.
-        source_ap:  Radius of the circular aperture.
+        image:      (str) name of the .fits file with the image
+        sources:    table with the Positions of the sources in the image.
+        source_ap:  Radius of the circular aperture. Units are pixels if coord
+                    = 'xy' or arcsec if coord = 'radec'
         sky_ap:     inner radius of the annulus to calculate the sky.
         delta_ann:  Width of the annulus to calculate the sky
-        centroid:   (Bolean) if True the source position centroid are
+        centroid:   (Boolean) if True the source position centroid are
                     recalculated.
+        sky_coord   (str) type of coordinates to use, either xy or radec.
+                    If xy sources table must have 'x' and 'y' cols.
+                    If ra dec sources table must to have 'ra' and 'dec' cols.
+        show_plot:  (Boolean) If True a plot of the aperture is displayed.
     Output:
         phot_table: Table with the resulting photometry
     """
-    # read list of sources
-    sources = Table.read(sources_pos, format='ascii', names=['x','y'])
 
-    # python index starts at zero
-    sources['x'] -= 1
-    sources['y'] -=1
+    data = fits.getdata(image)
+    x, y = sources.colnames
+
+    if coords == 'radec':
+        sources = SkyCoord(sources[x], sources[y], unit=('hourangle', 'deg'),
+                           **kargs)
+        # convert to pixel coordinates
+        header = fits.getheader(image)
+        wcs = WCS(header)
+        sources = Table(sources.to_pixel(wcs), names=[x, y])
+    elif coords == 'xy':
+        warnings.warn('Image pixels start at 1 while python index stats at 0',
+                      AstropyUserWarning)
+        # Check if the following correction is necessary
+        # sources[x] -= 1
+        # sources[y] -= 1
 
     if centroid:
-        sources = centroid_sources(image, sources['x'], sources['y'])
+        sources = centroid_sources(data, sources[x], sources[y], box_size=30,
+                                   centroid_func='centroid_2dg')
+        sources = Table(sources, names=[x, y])
 
     # create apertures
-    source_aperture = CircularAperture((sources['x'],sources['y']), r =
-                                       source_ap)
+    sources = [(a, b) for a, b in zip(sources[x], sources[y])]
+    source_aperture = CircularAperture(sources, r=source_ap)
 
-    sky_aperture = CircularAnnulus((sources['x'],sources['y']), r_in = sky_ap
-                                   , r_out = sky_ap + delta_ann)
+    sky_aperture = CircularAnnulus(sources, r_in=sky_ap, r_out=sky_ap +
+                                   delta_ann)
+
+    if plot:
+        norm = simple_norm(data, 'sqrt', percent=99)
+        plt.imshow(data, norm=norm)
+        source_aperture.plot(color='white', lw=2)
+        sky_aperture.plot(color='red', lw=2)
 
     # get background using sigma clipped median
     annulus_masks = sky_aperture.to_mask(method='center')
     bkg_median = []
     for mask in annulus_masks:
-        annulus_data = mask.multiply(image)
+        annulus_data = mask.multiply(data)
         annulus_data_1d = annulus_data[mask.data > 0]
         _, median_sigclip, _ = sigma_clipped_stats(annulus_data_1d)
         bkg_median.append(median_sigclip)
     bkg_median = np.array(bkg_median)
 
-
-    phot_table = aperture_photometry(image, source_aperture)
+    phot_table = aperture_photometry(data, source_aperture)
     phot_table['annulus_median'] = bkg_median
     phot_table['aper_bkg'] = bkg_median * source_aperture.area()
     phot_table['aper_sum_bkgsub'] = phot_table['aperture_sum'] - \
-                                    phot_table['aper_bkg']
-
-    # norm = simple_norm(image, 'sqrt', percent=99)
-    # plt.imshow(image, norm=norm)
-    # source_aperture.plot(color='white', lw=2)
-    # sky_aperture.plot(color='red', lw=2)
+        phot_table['aper_bkg']
 
     return phot_table
